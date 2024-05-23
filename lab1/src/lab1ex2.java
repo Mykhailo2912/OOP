e0d0fbcfbca34b8dfe5c499e435efb44bc23699b
Mykhailo Berezhanskyi TR-13 <127953643+Mykhailo2912@users.noreply.github.com>
2024-05-23 20:29:33 +0200

    Update lab1ex2.java

--- lab1/src/lab1ex2.java
+++ lab1/src/lab1ex2.java
@@ -5,14 +5,14 @@ public class lab1ex2 {
         
         String word1 = "Мене";
         String word2 = "звати";
-        String word3 = "Олександр,";
+        String word3 = "Михайло,";
         String word4 = "я";
         String word5 = "люблю";
         String word6 = "пити";
-        String word7 = "чай";
+        String word7 = "енергетики";
         String word8 = "та";
-        String word9 = "подорожувати";
-        String word10 = "Україною";
+        String word9 = "відвідувати";
+        String word10 = "навчання";
 
         System.out.println(word1 + " " + word2 + " " + word3 + " " + word4 + " " + word5 + " " + word6 + " " + word7 + " " + word8 + " " + word9 + " " + word10);
     }
